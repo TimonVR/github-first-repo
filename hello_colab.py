fa37bc993830dface45fa27d3c334211dd5a8c4b
TimonVR <timon.vanreeth@student.uantwerpen.be>
2026-06-07 19:46:33 +0000

    Updated the hello_colab script with a new line

--- hello_colab.py
+++ hello_colab.py
@@ -1,3 +1,5 @@
 print('Hello from Google Colab!')
 
 print('I just updated this file from Colab!')
+print('I just updated this file from Colab!')
+print('We just caught a Zenolax in the Derby of Eusseleir)
